1642d109735b6a792b9ebfa7949ee3a54c8734db
JobJonah <94242492+JobJonah@users.noreply.github.com>
2024-05-23 15:48:29 +0200

    number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,63 +1,72 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+
+INTRO(){
 echo "Enter your username:"
 read USERNAME
-GAME_INTRO(){
-USER_RECORD=$($PSQL "SELECT best_game, games_played FROM usernames WHERE username = '$USERNAME'")
-if [[ -z $USER_RECORD ]]
+USERNAME_ID=$($PSQL "SELECT user_id FROM usernames WHERE username='$USERNAME'")
+if [[ -z $USERNAME_ID ]]
 then
  echo "Welcome, $USERNAME! It looks like this is your first time here."
  INSERT_USER=$($PSQL "INSERT INTO usernames (username) VALUES ('$USERNAME')")
 else
- echo "$USER_RECORD" | while read BEST_GAME BAR GAMES_PLAYED
- do
+ RECORD=$($PSQL "SELECT best_game, games_played FROM record WHERE user_id=$USERNAME_ID")
+ read -r BEST_GAME BAR GAMES_PLAYED <<< $RECORD
  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
- done
+fi
+
+#GAME START
+SECRET_NUMBER=$(($RANDOM % 1000 + 1))
+echo $SECRET_NUMBER
+USERNAME_ID=$($PSQL "SELECT user_id FROM usernames WHERE username='$USERNAME'")
+NUMBER_OF_GUESSES=0
+echo "Guess the secret number between 1 and 1000:"
+}
+
+INPUT(){
+ read GUESSED_NUMBER
+ VERIFY
+}
+VERIFY(){
+if [[ ! $GUESSED_NUMBER =~ ^[0-9]+$ ]]
+then
+ echo "That is not an integer, guess again:"
+ INPUT
+else
+ NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
+ TEST
 fi
 }
-GAME_INTRO
 
-USERNAME_ID=$($PSQL "SELECT user_id FROM usernames WHERE username='$USERNAME'")
-
-#GAME START AND UPDATE
-SECRET_NUMBER=$(($RANDOM % 1000 + 1))
-#update played_games
-UPDATE_GAMES_PLAYED=$($PSQL "UPDATE usernames SET games_played = $GAMES_PLAYED + 1 WHERE user_id = $USERNAME_ID")
-INSERT_GAME=$($PSQL "INSERT INTO games (user_id) VALUES ($USERNAME_ID) RETURNING game_id")
-GAME_ID=$($PSQL "SELECT game_id FROM games WHERE user_id =$USERNAME_ID ORDER BY game_id DESC LIMIT 1")
-GUESS_GAME(){
- read GUESSED_NUMBER
- if [[ ! $GUESSED_NUMBER =~ ^[0-9]+$ ]]
+TEST(){
+ if [[ $GUESSED_NUMBER > $SECRET_NUMBER ]]
  then
-  GUESS_MESSAGE "That is not an integer, guess again:"
- else
-  INSERT_GUESS=$($PSQL "INSERT INTO guess (game_id, guessed_number) VALUES ($GAME_ID, $GUESSED_NUMBER)")
-  if [[ $GUESSED_NUMBER = $SECRET_NUMBER ]]
-  then
-   NUMBER_OF_GUESSES=$($PSQL "SELECT COUNT(*) FROM guess WHERE game_id = $GAME_ID")
-   NUMBER_OF_GUESSES_FORMATTED=$(echo $NUMBER_OF_GUESSES | sed -E 's/ //g')
-   #update games table
-   UPDATE_GAMES=$($PSQL "UPDATE games SET number_of_guesses = $NUMBER_OF_GUESSES_FORMATTED WHERE game_id = $GAME_ID")
-   #update usernames table
-   BEST_GAME=$(echo $($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE user_id = $USERNAME_ID") | sed -E 's/ //g')
-   INSERT_BEST_GAME=$($PSQL "UPDATE usernames SET best_game = $BEST_GAME WHERE user_id = $USERNAME_ID")
-   INSERT_RECORD=$($PSQL "INSERT INTO record (game_id, number_of_guesses) VALUES ($GAME_ID, $NUMBER_OF_GUESSES_FORMATTED)")
-   echo "You guessed it in $NUMBER_OF_GUESSES_FORMATTED tries. The secret number was $SECRET_NUMBER. Nice job!"
-  elif [[ $GUESSED_NUMBER > $SECRET_NUMBER ]]
-   then
-   GUESS_MESSAGE "It's lower than that, guess again:"
-  else 
-   GUESS_MESSAGE "It's higher than that, guess again:"
-  fi 
+  echo "It's lower than that, guess again:"
+  INPUT
+ elif [[ $GUESSED_NUMBER < $SECRET_NUMBER ]]
+ then
+  echo "It's higher than that, guess again:"
+  INPUT
+ elif [[ $GUESSED_NUMBER = $SECRET_NUMBER ]]
+ then
+  END
  fi
 }
-GUESS_MESSAGE(){
- if [[ $1 ]] 
- then 
-  echo $1
- else
-  echo "Guess the secret number between 1 and 1000:"
- fi
- GUESS_GAME
+
+END(){
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+#update record table
+BEST_GAME=$($PSQL "SELECT best_game FROM record WHERE user_id=$USERNAME_ID")
+if [[ -z $BEST_GAME ]]
+then
+ GAMES_PLAYED=1
+ BEST_GAME=$NUMBER_OF_GUESSES
+ INSERT_RECORD=$($PSQL "INSERT INTO record (user_id, best_game, games_played) VALUES ($USERNAME_ID, $BEST_GAME, $GAMES_PLAYED)")
+else
+ BEST_GAME=$(( $BEST_GAME < $NUMBER_OF_GUESSES ? $BEST_GAME: $NUMBER_OF_GUESSES ))
+ UPDATE_RECORD1=$($PSQL "UPDATE record SET games_played = games_played + 1 WHERE user_id= $USERNAME_ID")
+ UPDATE_RECORD2=$($PSQL "UPDATE record SET best_game = $BEST_GAME WHERE user_id= $USERNAME_ID")
+fi
 }
-GUESS_MESSAGE
+INTRO
+INPUT
